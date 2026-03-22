998714631411fc40234f80868902ce87b58fe28a
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2026-03-22 10:45:45 -0300

    Initial Commit

---
+++ VideoEditorKit/VideoEditorKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorKitApp.swift
+//  VideoEditorKit
+//
+//  Created by Didi on 22/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoEditorKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoEditorKitTests/VideoEditorKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideoEditorKitTests.swift
+//  VideoEditorKitTests
+//
+//  Created by Didi on 22/03/26.
+//
+
+import Testing
+@testable import VideoEditorKit
+
+struct VideoEditorKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
